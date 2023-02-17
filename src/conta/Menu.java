66f0aed25791ddd27e07c03ca6543304ecd2e364
Caroline Viana <carolviana-21@hotmail.com>
2023-02-17 16:59:02 -0300

    Fazendo a classe conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,43 +2,49 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.Conta;
+import conta.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		
+		Conta c1 = new Conta(1, 123, 1, "Carol", 100.0f);
+		c1.visualizar();
+		c1.setSaldo(1100.0f);
+		System.out.println(c1.getSaldo());
 		int opcao;
-		
-		while(true) {
-			System.out.println("*****************************************************");
+
+		while (true) {
+			System.out.println(Cores.TEXT_BLUE_BOLD + "*****************************************************");
 			System.out.println("                                                     ");
 			System.out.println("                BANCO DO BRAZIL COM Z                ");
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("                                                     ");
-			System.out.println("            1 - Criar Conta                          ");
-			System.out.println("            2 - Listar todas as Contas               ");
-			System.out.println("            3 - Buscar Conta por Numero              ");
-			System.out.println("            4 - Atualizar Dados da Conta             ");
-			System.out.println("            5 - Apagar Conta                         ");
-			System.out.println("            6 - Sacar                                ");
-			System.out.println("            7 - Depositar                            ");
-			System.out.println("            8 - Transferir valores entre Contas      ");
-			System.out.println("            9 - Sair                                 ");
+			System.out.println("        >-< 1 - Criar Conta                          ");
+			System.out.println("        >-< 2 - Listar todas as Contas               ");
+			System.out.println("        >-< 3 - Buscar Conta por Numero              ");
+			System.out.println("        >-< 4 - Atualizar Dados da Conta             ");
+			System.out.println("        >-< 5 - Apagar Conta                         ");
+			System.out.println("        >-< 6 - Sacar                                ");
+			System.out.println("        >-< 7 - Depositar                            ");
+			System.out.println("        >-< 8 - Transferir valores entre Contas      ");
+			System.out.println("        >-< 9 - Sair                                 ");
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     ");
 
 			opcao = leia.nextInt();
-			
-			if(opcao == 9) {
+
+			if (opcao == 9) {
 				System.out.println("\nBanco do Brazil com Z - O seu futuro começa aqui!");
 				leia.close();
 				System.exit(0);
 			}
-			
-			switch(opcao) {
+
+			switch (opcao) {
 			case 1:
 				System.out.println("Criar Conta\n\n");
 				break;
@@ -67,6 +73,7 @@ public class Menu {
 				System.out.println("\nOpção Inválida!\n");
 				break;
 			}
+
 		}
 
 	}
